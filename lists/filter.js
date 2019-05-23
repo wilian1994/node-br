b07d6c7a97f6a4785b0f175d33ccff851de8e0e5
Wilian da silva <wiliandasilvaoliveira1994@gmail.com>
2019-05-23 05:43:15 -0300

    Trabalhando com filter

---
+++ lists/filter.js
@@ -0,0 +1,24 @@
+const { obterPessoas }  = require('./service'); //sacada do objeto que retorna eu quero que retorne somente o obterPessoa
+
+
+async function main() {
+    try{
+        const {results} = await obterPessoas(`a`)
+
+        const familiaLars = results.filter(function(item){
+            // POR PADRÃO PRECISA RETORNA UM BOOLEANO
+            // PARA INFORMAR SE DEVE MANTER OU REMOVER DA LISTA
+
+            const result = item.name.toLowerCase().indexOf('lars') !== -1
+            return result;
+        })
+
+        const names = familiaLars.map(pessoa => pessoa.name);
+        console.log(`Names: ${names}`)
+
+    } catch (error){
+        console.log('DEU RUIM', error)
+    }
+}
+
+main()
