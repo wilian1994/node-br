d9d335a69a069d3765698b01831670ee1a44cade
Wilian da silva <wiliandasilvaoliveira1994@gmail.com>
2019-05-23 06:01:10 -0300

    Finalizando o módulo 2 - trabalhando com listas

--- lists/filter.js
+++ lists/filter.js
@@ -1,24 +1,57 @@
-const { obterPessoas }  = require('./service'); //sacada do objeto que retorna eu quero que retorne somente o obterPessoa
+const {
+    obterPessoas
+} = require('./service')
 
+/*
 
-async function main() {
-    try{
-        const {results} = await obterPessoas(`a`)
+ const item = {
+     nome: 'Erick',
+     idade: 12,
 
-        const familiaLars = results.filter(function(item){
-            // POR PADRÃO PRECISA RETORNA UM BOOLEANO
-            // PARA INFORMAR SE DEVE MANTER OU REMOVER DA LISTA
-            // TRAZ TOODO MUNDO QUE É DA FAMILIA
-            const result = item.name.toLowerCase().indexOf('lars') !== -1
-            return result;
-        })
+ }
 
-        const names = familiaLars.map(pessoa => pessoa.name);
-        console.log(`Names: ${names}`)
-
-    } catch (error){
-        console.log('DEU RUIM', error)
+ const { nome , idade } = item
+ console.log(nome, idade)
+*/
+Array.prototype.meuFilter = function (callback) {
+    const lista = []
+    for (index in this) {
+        const item = this[index]
+        const result = callback(item, index, this)
+        // 0, "", null, undefined === false
+        if (!result) continue;
+        lista.push(item)
     }
+    return lista;
 }
 
+async function main() {
+    try {
+        const {
+            results
+        } = await obterPessoas(`a`)
+
+        // const familiaLars = results.filter(function (item) {
+        //     // por padrão precisa retornar um booleano
+        //     // para informar se deve manter ou remover da lista
+        //     // false > remove da lista
+        //     // true > mantem
+        //     // não encontrou = -1
+        //     // encontrou = posicaoNoArray
+        //     const result = item.name.toLowerCase().indexOf(`lars`) !== -1
+        //     return result
+        // })
+        const familiaLars = results.meuFilter((item, index, lista) => {
+            console.log(`index: ${index}`, lista.length)
+            return item.name.toLowerCase().indexOf('lars') !== -1
+        })
+
+        const names = familiaLars.map((pessoa) => pessoa.name)
+        console.log(names)
+
+    } catch (error) {
+        console.error('DEU RUIM', error)
+    }
+
+}
 main()
